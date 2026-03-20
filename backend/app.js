4e471149cbcda1a1d1bee8bdd1971f0b8bc9deec
kang8284 <gustlr6336@gmail.com>
2026-03-21 00:35:00 +0900

    벡엔드 데이터베이스 연결 확인

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); // 이미 설치한 상태
 const app = express();
 require('dotenv').config();
 const PORT = 5000;
 
-// JSON 파싱 + CORS 설정
+// body parsing
 app.use(express.json());
-app.use(cors());
 
-// 라우터 연결
+// CORS 허용
+app.use(cors({
+  origin: 'http://localhost:3000', // React 앱 주소
+  methods: ['GET', 'POST', 'DELETE'],
+  credentials: true
+}));
+
+// 라우트
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
